a53589ea6210227d9817ffd804443d5723eaf746
Cristian Crudu <Cristian.Crudu@endava.com>
2022-10-05 10:53:47 +0300

    Added like/dislike methods to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,38 @@
 import { defineStore } from "pinia";
+import { SongType } from "../types/SongType";
 import songFile from "./youtube_songs_list.json";
 
-const songList = songFile.items.map((x, index): any => {
+const songList: SongType[] = songFile.items.map((x, index): any => {
     return { songData: x, likes: index, dislikes: 0 };
 });
 
 export const useSongsStore = defineStore("songs", {
     state: () => ({ songs: songList }),
+    actions: {
+        findSong(id: string): SongType {
+            return this.songs.find(
+                (song) => song.songData.id === id
+            ) as SongType;
+        },
+        likeASong(id: string) {
+            const song = this.findSong(id);
+            song.likes++;
+        },
+        dislikeASong(id: string) {
+            const song = this.findSong(id);
+            song.dislikes++;
+        },
+
+        removeLike(id: string) {
+            const song = this.findSong(id);
+            song.likes--;
+        },
+
+        removeDislike(id: string) {
+            const song = this.findSong(id);
+            song.dislikes--;
+        },
+    },
     getters: {
         topTenPopular: (state) =>
             state.songs
